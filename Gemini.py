f16d3a89a7bc8e2bba6090839acb3b83ff09fb3f
DevsmileGod <godsmilerxiaomin@proton.me>
2026-02-04 13:03:51 -0300

    FIX: Fixing the import of the GenAi Library in ./Gemini.py

--- Gemini.py
+++ Gemini.py
@@ -48,7 +48,7 @@ Assumptions & Notes:
 
 import atexit  # For playing a sound when the program finishes
 import datetime  # For getting the current date and time
-import google.generativeai as genai  # Import the Google AI Python SDK
+import google.genai as genai  # Import the Google AI Python SDK
 import os  # For running a command in the terminal
 import platform  # For getting the operating system name
 import sys  # For system-specific parameters and functions
