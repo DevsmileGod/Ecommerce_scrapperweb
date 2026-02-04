c33b34cd86a08f4dc81ac2bc78af1e96787c2b38
DevsmileGod <godsmilerxiaomin@proton.me>
2026-02-04 11:31:32 -0300

    REFACTOR: Updating the main function in ./main.py

--- main.py
+++ main.py
@@ -297,6 +297,8 @@ def main():
         os.path.abspath(OUTPUT_DIRECTORY), OUTPUT_DIRECTORY.replace(".", "")
     )  # Create the output directory
     
+    verify_filepath_exists(INPUT_FILE)  # Verify if the input file exists
+
     finish_time = datetime.datetime.now()  # Get the finish time of the program
     print(
         f"{BackgroundColors.GREEN}Start time: {BackgroundColors.CYAN}{start_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Finish time: {BackgroundColors.CYAN}{finish_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Execution time: {BackgroundColors.CYAN}{calculate_execution_time(start_time, finish_time)}{Style.RESET_ALL}"
