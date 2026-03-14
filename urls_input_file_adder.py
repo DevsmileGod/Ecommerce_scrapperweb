07d9e6ad6df21a215c9a3116929e747974d432c9
DevsmileGod <godsmilerxiaomin@proton.me>
2026-03-13 22:40:35 -0300

    REFACTOR: Removing the double empty lines in the Welcome Message in ./urls_input_file_adder.py

--- urls_input_file_adder.py
+++ urls_input_file_adder.py
@@ -382,7 +382,7 @@ def main():
 
     print(
         f"{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}URLs Input File Adder{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
-        end="\n\n",
+        end="\n",
     )  # Output the welcome message
     start_time = datetime.datetime.now()  # Get the start time of the program
     
