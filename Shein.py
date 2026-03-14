627b8ae281783396ecb9a47a3726fd01d62417bf
DevsmileGod <godsmilerxiaomin@proton.me>
2026-03-13 22:28:11 -0300

    REFACTOR: Removing the console log clearing at the Welcome message in ./Shein.py

--- Shein.py
+++ Shein.py
@@ -1876,7 +1876,7 @@ def main():
     """
 
     print(
-        f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Shein Scraper{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
+        f"{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Shein Scraper{BackgroundColors.GREEN} program!{Style.RESET_ALL}",
         end="\n",
     )
     
